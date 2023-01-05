772417cb67476bd6ff262a162de59e1a839133df
giselars <gisela-rs@outlook.com>
2023-01-05 11:42:39 +0100

    :wrench: add config

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calendar-App MERN back
+
+Stack: Node - Express - Mongo
+
+Ejercicio práctico para el curso de React: De cero a experto ( Hooks y MERN ) de Fernando Herrera, https://www.udemy.com/course/react-cero-experto
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require( 'dotenv' ).config();
+
+console.log( process.env );
+
+//Crear el servidor de express
+const app = express();
+
+
+//Directorio Público
+app.use( express.static( 'public' ));
+
+
+//Rutas
+//TODO: auth // crear, login, renew
+//TODO: CRUD: Eventos
+
+//Escuchar peticiones
+app.listen( process.env.PORT, () =>{
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
